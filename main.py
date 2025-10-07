f00d9ba9ecd373092e6d3ce532f89852a21361c1
Akshay Vardhan Burra <142847672+AkshayVardhanBurra@users.noreply.github.com>
2025-10-07 16:57:46 -0700

    Added admin portal for user

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def saveContacts(contacts, contactFile=CONTACTS_FILE):
     
     with open(contactFile, "w") as file:
         for contact in contacts:
-            file.write(str(contact))
+            file.write(str(contact) + "\n")
         
 
 def printAllContacts(contacts):
@@ -46,6 +46,67 @@ saveContacts(contacts)
 
 
 
+
+
+
+
+
+
+
+#returns a number
+def get_valid_choice():
+    index = int(input("Enter the index: "))
+
+    while index > len(contacts) or index < 1:
+        index = int(input("Enter a proper index: "))
+    
+    return index - 1
+
+def updateContact(contact:Contact):
+    contact.name = input("Enter a name: ")
+    contact.phoneNumber = int(input("Enter a phone number: "))
+    contact.mail = input("Enter a mail: ")
+    contact.countryCode = input("Enter a country code: ")
+    contact.hasSMS = input("Has SMS? (y/n)") == "y"
+    contact.hasWhatsApp = input("Has WhatsApp (y/n)") == "y"
+
+def createContact():
+    name = input("Enter a name: ")
+    phoneNumber = int(input("Enter a phone number: "))
+    mail = input("Enter a mail: ")
+    countryCode = input("Enter a country code: ")
+    hasSMS = input("Has SMS? (y/n)") == "y"
+    hasWhatsApp = input("Has WhatsApp (y/n)") == "y"
+
+    return Contact(name, phoneNumber, hasWhatsApp, hasSMS, mail, 0, countryCode)
+
+
+user_choice = input("VIEW, UPDATE, DELETE, or CREATE: ")
+while user_choice != "quit":
+
+    if user_choice == "VIEW":
+        printAllContacts(contacts)
+    
+    elif user_choice == "UPDATE" and len(contacts) > 0:
+        printAllContacts(contacts)
+        valid_index = get_valid_choice()
+        updateContact(contacts[valid_index])
+        saveContacts(contacts, CONTACTS_FILE)
+    elif user_choice == "CREATE":
+        new_contact = createContact()
+        contacts.append(new_contact)
+        saveContacts(contacts, CONTACTS_FILE)
+        
+    elif user_choice == "DELETE" and len(contacts) > 0:
+        printAllContacts(contacts)
+        valid_index = get_valid_choice()
+        contacts.pop(valid_index)
+        saveContacts(contacts, CONTACTS_FILE)
+       
+    else:
+        print("Something must have been wrong with the input!")
+
+    user_choice = input("VIEW, UPDATE, DELETE, or CREATE: ")
 
 
 
